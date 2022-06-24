db820dd28a84339b2c88d951c722c670b5c28365
Ibikunle Jamiu <ibidotun@gmail.com>
2022-06-24 21:18:51 +0100

    Prevented users from joining same room

--- index.js
+++ index.js
@@ -13,17 +13,49 @@ const io = new Server(server, {
   }
 })
 
+let clientsdata = []
+let activerooms = []
 
 io.on('connection', (socket) => {
-  console.log('A user connected: ' + socket.id);
+  clientsdata.push(socket.id)
+  console.log(`User ${socket.id} joined. A total of ${clientsdata.length} users are connected`);
 
-  socket.on('sendMessage', ({message, id}) => {
-    console.log({message, id});
-    socket.broadcast.emit('receiveMessage', {message, id})
+  socket.emit('me', socket.id)
+
+  socket.on('createRoom', (randomNum) => {
+    socket.join(randomNum)
+    activerooms.push({roomid: randomNum, connectedusers: [socket.id]})
+    console.log(activerooms);
+  })
+  
+  socket.on('getRooms', (roomCode) => {
+    let isOld = false
+    activerooms.map((e) => {
+      if (e.connectedusers.includes(socket.id)) isOld = true
+    })
+    let auth = activerooms.find(e => e.roomid === roomCode)
+    if (auth && !isOld) {
+      (activerooms[activerooms.indexOf(auth)] = {roomid: auth.roomid, connectedusers: [...auth.connectedusers, socket.id]})
+      socket.join(roomCode)
+      socket.emit('joinStatus', {isJoined: 'Success'})
+      console.log(activerooms);
+    } else {
+      socket.emit('joinStatus', {isJoined: 'Failure'})}
+  })
+
+  socket.on('sendMessage', ({message, id, roomid}) => {
+    console.log({message, id, roomid});
+    socket.to(roomid).emit('receiveMessage', {message, id})
   })
 
   socket.on('disconnect', () => {
-    console.log('A user disconnected')
+    let newdata = clientsdata.filter(e => e !== socket.id)
+    clientsdata = newdata
+    activerooms.map(room => {
+      room.connectedusers = room.connectedusers.filter(e => e !== socket.id)
+    })
+    activerooms = activerooms.filter(e => e.connectedusers.length !== 0)
+    console.log(`A user disconnected. ${clientsdata.length} users are currently connected.`)
   })
 })
 
